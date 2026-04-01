8f5fdeacb2efd529252694dcf305a120c495c00e
Karan Singh <karan.devmail@gmail.com>
2026-04-01 13:23:06 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://vidhi-desk.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
