33b762cf804c7b654b9ae9b6633060523cfde47b
Karan Singh <karan.devmail@gmail.com>
2026-04-10 12:48:38 +0530

    Lead status name updated from "Not Interested" to "Declined" in LeadDetails.jsx

--- client/src/components/Leads/LeadDetails.jsx
+++ client/src/components/Leads/LeadDetails.jsx
@@ -422,7 +422,7 @@ function LeadDetails() {
                     >
                       <option value="Open">Open</option>
                       <option value="File Received">File Received</option>
-                      <option value="Declined">Not Interested</option>
+                      <option value="Declined">Declined</option>
                     </select>
                   </div>
                   <button
